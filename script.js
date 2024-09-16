698c480756f5d51a98edf2bcef7266630a1b30f6
The _IsaacAgboola <isaacadeyemi9@gmail.com>
2024-09-16 08:20:13 +0100

    db connected

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hiii')
+const mongoose = require('mongoose')
+const User = require ("./User")
+mongoose.connect("mongodb://localhost/testdb");
+
+const user =  new User({
+    name: 'isaac',
+    age : 27
+})
+
+run();
+
+async function run(){
+     await user.save()
+     console.log(user)
+}
+
